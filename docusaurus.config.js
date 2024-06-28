537ccac785a0890daab4e29161437b9b9ca64d41
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2024-06-28 09:50:09 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Monni',
+  tagline: 'Monni, perhaps the only pet you need. Keeps you cozy and parasite free.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
